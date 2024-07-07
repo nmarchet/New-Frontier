527f98a1b418aaad293d5a242e2e45f85b9d8b72
Nick Marchetti <nmarchet@gmail.com>
2024-07-06 20:00:33 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    titleImage: "/images/new-frontier.jpg"
+    /**pageTitle: "🪴 Quartz 4.0",*/
     enableSPA: true,
     enablePopovers: true,
     analytics: {
