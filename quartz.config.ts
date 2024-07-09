a3b998d135d00344b3e07d958f30104bb0bd6658
Nick Marchetti <nmarchet@gmail.com>
2024-07-09 16:04:24 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Goblin One",
+        header: "Asset",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
