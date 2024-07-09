989c3603d004885aab44906e5f0e12ea534cdf05
Nick Marchetti <nmarchet@gmail.com>
2024-07-09 16:17:58 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "D&D New Frontier",
+    pageTitle: "D&D | New Frontier",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
