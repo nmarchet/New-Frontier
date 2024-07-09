699d18768c9c08ca16acd0106b423ab35b35f5ad
Nick Marchetti <nmarchet@gmail.com>
2024-07-09 16:08:48 -0400

    Update quartz.config.ts
    Previously "Source Sans Pro"

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Asset",
-        body: "Source Sans Pro",
+        body: "Newsreader",
         code: "IBM Plex Mono",
       },
       colors: {
